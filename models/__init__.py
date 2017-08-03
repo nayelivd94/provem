a705cf32eaa5d2072e992499374f70c7f5dfeacd
Nay3li <nayeli.valencia@xmarts.com.mx>
2017-08-03 17:06:56 -0500

    Modulo con reportes de contrato Provem

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+import amount_to_text
+import models
---
+++ models/models.py
@@ -0,0 +1,43 @@
+## -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, _, tools
+from openerp.exceptions import UserError, RedirectWarning, ValidationError
+from datetime import datetime, date, time,timedelta
+import xlrd
+import shutil
+import logging
+import time
+import amount_to_text
+_logger = logging.getLogger(__name__)
+
+class HrEmployee(models.Model):
+	_inherit ='hr.employee'
+  	edad= fields.Integer(string="Edad", compute="_compute_days")
+class hremployees(models.Model):
+    _inherit ='hr.employee'
+
+    @api.one
+    def _compute_days(self):
+        if str(self.birthday) <> 'False':
+            # _logger.info(_("entrooooooooooo  \n\n \n%s") % (str(self.validity_date)))
+            fecha = str(self.birthday) + ' 00:00:00'
+            cumple = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S').date()
+            _logger.info(_("cumple: \n\n \n%s") % (cumple))
+            i= datetime.now()
+            hoy= (str(i.year)+'-'+str(i.month,)+'-'+ str(i.day) + ' 00:00:00')
+            fecha_hoy = datetime.strptime(hoy, '%Y-%m-%d %H:%M:%S').date()
+            _logger.info(_("fecha hoy: \n\n \n%s") % (fecha_hoy))
+            total = fecha_hoy - cumple
+            years = str(int(total.days / 365))
+            _logger.info(_("Edad: \n\n \n%s") % (years))
+            self.edad = years
+class hrcontract(models.Model):
+    _inherit ='hr.contract'
+
+    @api.one
+    @api.depends('wage')
+    def _get_amount_to_text(self):
+        _logger.info(_("ENNTRO a monto texto "))
+        self.amount_to_text = amount_to_text.get_amount_to_text(self, self.wage,'MXN')
+    amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True,
+                                 help='Amount of the invoice in letter')
